6ab0ba115466ab1808f5f2ba212dc69d98e86544
Felix Schill <felix.schill@hydromea.com>
2020-05-20 13:02:07 +0200

    updated initial sizes

--- mincam.py
+++ mincam.py
@@ -68,7 +68,7 @@ class CAMGui(QtGui.QSplitter):
         self.updateGeometry()
         self.resize(1600,  900)
         ## Display the widget as a new window
-        self.setSizes([600,4000])
+        self.setSizes([700,4000])
         self.setStretchFactor(1,1)
 
 
